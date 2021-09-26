369f045ddd836a02bf977cf35ed33385dc4602c9
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-09-26 18:26:06 +0300

    Model Created,Fragments Coded, Room database CREATED

---
+++ app/src/main/java/com/example/artbooktesting/roomdb/ArtDatabase.kt
@@ -0,0 +1,9 @@
+package com.example.artbooktesting.roomdb
+
+import androidx.room.Database
+
+
+@Database(entities = [Art::class],version = 1)
+abstract class ArtDatabase {
+    abstract fun artDao():ArtDao
+}
