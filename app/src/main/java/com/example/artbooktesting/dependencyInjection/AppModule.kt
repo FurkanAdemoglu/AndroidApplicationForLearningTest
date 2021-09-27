53e6eb1887faa21fdfdd2205c4f099c9aa46335f
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-09-27 09:34:20 +0300

    Fragments,Factories and Adapters Coded

--- app/src/main/java/com/example/artbooktesting/dependencyInjection/AppModule.kt
+++ app/src/main/java/com/example/artbooktesting/dependencyInjection/AppModule.kt
@@ -3,7 +3,13 @@ package com.example.artbooktesting.dependencyInjection
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.artbooktesting.R
 import com.example.artbooktesting.api.RetrofitAPI
+import com.example.artbooktesting.repo.ArtRepository
+import com.example.artbooktesting.repo.ArtRepositoryInterface
+import com.example.artbooktesting.roomdb.ArtDao
 import com.example.artbooktesting.roomdb.ArtDatabase
 import com.example.artbooktesting.util.Util.BASE_URL
 import dagger.Module
@@ -41,4 +47,16 @@ object AppModule {
             .build()
             .create(RetrofitAPI::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun injectNormalRepo(dao:ArtDao,api:RetrofitAPI)=ArtRepository(dao,api) as ArtRepositoryInterface
+
+    @Singleton
+    @Provides
+    fun injectGlide(@ApplicationContext context: Context)= Glide.with(context)
+        .setDefaultRequestOptions(
+            RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+        )
 }
